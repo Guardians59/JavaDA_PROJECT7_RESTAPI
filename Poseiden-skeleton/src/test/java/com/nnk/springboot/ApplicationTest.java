73f834297a4bebf83500a747147789061bbad55c
Dylann Vandenbussche <vandenbussche.dylann@gmail.com>
2022-03-02 19:54:57 +0100

    Application main test

---
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/ApplicationTest.java
@@ -0,0 +1,16 @@
+package com.nnk.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+public class ApplicationTest {
+
+    @Test
+	void contextLoads() {
+	}
+	
+	@Test
+	public void contextTest() {
+	    Application.main(new String[] {});
+	}
+}
